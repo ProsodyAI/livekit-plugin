50391d9e7862efb9a07ebf268a22f484578563bd
Jacob Chaffin <jchaffin@ucla.edu>
2026-08-09 14:54:43 -0700

    Format the GatewayEvent union on one line for the ruff format gate

--- livekit/plugins/prosodyai/full_duplex.py
+++ livekit/plugins/prosodyai/full_duplex.py
@@ -115,12 +115,7 @@ class TextEvent:
 
 
 GatewayEvent = (
-    ReadyEvent
-    | TextEvent
-    | IdentityEvent
-    | TranscriptEvent
-    | ModelEvent
-    | ConversationEvent
+    ReadyEvent | TextEvent | IdentityEvent | TranscriptEvent | ModelEvent | ConversationEvent
 )
 
 
